2dd2380c1d455a45cff5b38ea56bd40f17dd4848
Trym Kristian <chilled-1337@hotmail.com>
2018-10-09 23:10:04 +0200

    Moving on with the course

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,1 +1,17 @@
 import React from 'react';
+
+import classes from './Burger.css';
+import BurgerIngredient  from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+    return (
+        <div className={classes.burger}>
+            <BurgerIngredient type="bread-top"/>
+            <BurgerIngredient type="salad"/>
+            <BurgerIngredient type="meat"/>
+            <BurgerIngredient type="bread-bottom"/>
+        </div>
+    );
+};
+
+export default burger;
